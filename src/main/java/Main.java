d2ab131d3aa1bd6a0004eb136b344f196ada83de
Zhephyr54 <ale.lanoix@gmail.com>
2017-03-16 14:44:34 +0100

    Ajout des tests pour la classe Position

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,15 +5,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		Game game = new Game(new EpochXX());
-		System.out.println(game);
 		game.startGame(Game.COMPUTER);
-		System.out.println("\n"+game);
 		for (int i = 0 ; i < 5 ; i++) {
 			game.receiveClickEvent(i, 0);
-			System.out.println("\n"+game);
 		}
+		// tirs
 		game.receiveClickEvent(0, 0);
 		System.out.println("\n"+game);
+		game.receiveClickEvent(0, 1);
+		System.out.println("\n"+game);
+		game.receiveClickEvent(0, 2);
+		System.out.println("\n"+game);
+		game.receiveClickEvent(0, 3);
+		System.out.println("\n"+game);
+		game.receiveClickEvent(0, 4);
+		System.out.println("\n"+game);
+
 	}
 
 }
